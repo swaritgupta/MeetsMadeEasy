f15fcdddd7e8296c32c14926b3c3afb82378eb7a
Swarit Gupta <swaritgpt17@gmail.com>
2026-03-09 20:02:37 +0530

    Initialised project

---
+++ meets-made-easy/src/backend/main.ts
@@ -0,0 +1,35 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app/app.module';
+const figlet = require('figlet');
+import * as dotenv from 'dotenv';
+
+const banner = async (name: string) => {
+  return new Promise((resolve, reject) => {
+    figlet.text(name, (err: any, data: any) => {
+      if(err){
+        console.error("Something went wrong", err);
+        reject(err);
+        return;
+      }
+      console.log(""+data);
+      resolve(data);
+    });
+  });
+};
+
+async function bootstrap() {
+  dotenv.config({ path: '.env' });
+  const bannerVal = process.env.BANNER;
+  if(!bannerVal){
+    console.error("Banner went missing");
+    return;
+  }
+  await banner(bannerVal)
+  const app = await NestFactory.create(AppModule);
+  const PORT = process.env.PORT || 4000;
+  const HOST = process.env.HOST || '127.0.0.1';
+  await app.listen(Number(PORT), HOST);
+    console.log(`🚀 Server is running on http://${HOST}:${PORT}`);
+  console.log(`MeetsMadeEasy is initialized`);
+}
+bootstrap();
