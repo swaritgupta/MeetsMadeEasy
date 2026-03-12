21856843289cb5cae6eeb019794999d2607062a1
Swarit Gupta <swaritgpt17@gmail.com>
2026-03-12 15:54:05 +0530

    Added HttpUtils

---
+++ meets-made-easy/src/backend/utilities/HttpUtil.ts
@@ -0,0 +1,44 @@
+import { HttpService } from "@nestjs/axios";
+import { HttpException, HttpStatus } from "@nestjs/common";
+import { AxiosRequestConfig, AxiosResponse } from "axios";
+import { firstValueFrom } from "rxjs";
+
+
+export class HttpUtil{
+  constructor(private readonly httpService: HttpService){}
+
+  async get<T>(url: string, config?: AxiosRequestConfig): Promise<T>{
+    try{
+      const response: AxiosResponse<T> = await firstValueFrom(
+        this.httpService.get<T>(url, config)
+      );
+      return response.data;
+    }catch(err){
+      return this.handleError(err);
+    }
+  }
+
+  async post<T>(url: string, data: any, config?: AxiosRequestConfig): Promise<T>{
+    try{
+      const response: AxiosResponse<T> = await firstValueFrom(
+        this.httpService.post<T>(url, data, config)
+      );
+      return response.data;
+    }catch(err){
+      return this.handleError(err);
+    }
+  }
+
+  private handleError(error: any): never {
+    if(error.response){
+      throw new HttpException(
+        error.response.data || 'HTTP REQUEST FAILED',
+        error.response.status || HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+    throw new HttpException(
+      error.message || 'UNKNOWN HTTP ERROR',
+      HttpStatus.INTERNAL_SERVER_ERROR
+    );
+  }
+}
---
+++ meets-made-easy/src/backend/utilities/http-util.module.ts
@@ -0,0 +1,17 @@
+import { HttpModule } from '@nestjs/axios';
+import { Module } from '@nestjs/common';
+import { HttpUtil } from './HttpUtil';
+
+@Module({
+  imports: [
+    HttpModule.register({
+      timeout: 5000,
+      maxRedirects: 5,
+    })
+  ],
+  providers: [
+    HttpUtil
+  ],
+  exports: [HttpUtil]
+})
+export class HttpUtilModule {}
