38a636a255e551be4cf9448d2ca3bef2d8fef543
Swarit Gupta <swaritgpt17@gmail.com>
2026-03-14 21:16:00 +0530

    GeminiClient added

--- meets-made-easy/src/backend/utilities/GeminiClient.ts
+++ meets-made-easy/src/backend/utilities/GeminiClient.ts
@@ -1,7 +1,16 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 export class GeminiClient{
   private geminiClient;
   constructor(){
-    
+    const apiKey = process.env.GEMINI_API_KEY
+    if(!apiKey){
+      throw new Error('Api key is required for llm')
+    }
+    this.geminiClient = new GoogleGenerativeAI(apiKey)
+  }
+
+  public getObject(){
+    return this.geminiClient;
   }
 }
